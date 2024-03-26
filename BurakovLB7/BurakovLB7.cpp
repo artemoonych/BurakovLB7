03499817ecd8c825e20cf189117dd968e3d0f989
Artem <Artem@DESKTOP-27S6C4U>
2024-03-26 22:48:16 +0300

    Добавьте файлы проекта.

---
+++ BurakovLB7/BurakovLB7.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <map>
+#define MAX 50
+
+template <typename T>
+class Stack {
+  public:
+      void push(const T n) {
+          top++;
+          arr[top] = n;
+      }
+
+      T& pop() {
+          if (top >= 0) {
+              top--;
+              return arr[top + 1];
+          }
+          else
+              throw std::out_of_range("Стек пуст");
+      }
+
+      int size() const {
+          return top + 1;
+      }
+
+  private:
+    T arr[MAX];
+    int top = -1;
+};
+
+
+int main()
+{
+    std::map<char, int> prioryty = { {')', 0}, {'(', 1}, {'+', 2}, {'-', 2}, {'*', 3}, {'/', 3}, {'^',4}};
+
+    Stack<char> a;
+    char c;
+    while (std::cin.get(c) && c!='\n') {
+        if (c != ' ') {
+
+        }
+    }
+}
